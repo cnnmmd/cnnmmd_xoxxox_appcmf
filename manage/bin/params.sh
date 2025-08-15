c6f4dd66c057dbdeddcb48432379d718f5240d7a
azsgit <10238943+azsgit@users.noreply.github.com>
2025-08-15 21:36:38 +0900

    rename docker image

--- manage/bin/params.sh
+++ manage/bin/params.sh
@@ -1,4 +1,4 @@
+pthdoc="${pthtop}"/export/cnf/xoxxox/docenv_envcmf
 cnfimg="${pthtop}"/export/cnf/xoxxox/docenv_envcmf.txt
 cnfcmp="${pthtop}"/export/cnf/xoxxox/docker_appcmf.yml
-pthdoc="${pthtop}"/export/cnf/xoxxox/docker
-imgtgt='xoxxox_envcmf:001'
+imgtgt='xoxxox/envcmf:001'
